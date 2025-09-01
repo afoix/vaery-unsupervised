626d46fe3b147b16ebe8e58fb55a345c594fe74b
nicholasjchua <c.nicholas.j@gmail.com>
2025-08-31 21:22:31 -0400

    More work on salamander loader

--- vaery_unsupervised/dataloaders/sal-brain-loader.py
+++ vaery_unsupervised/dataloaders/sal-brain-loader.py
@@ -1,27 +1,46 @@
 import lightning as L
-from torch.utils.data import Dataset
+from torch import Tensor
+from torch.utils.data import Dataset, DataLoader
+from iohub import open_ome_zarr
+import numpy as np
+from monai.transforms import RandSpatialCropSamples
 
 from typing import Tuple, Union, List
 
 class SalBrainDataModule(L.LightningDataModule):
+    """
+    logic regarding interaction of data and training routines 
+    """
     
     def __init__(self, 
+                 data_path: str="/home/jnc2161/mbl/registered_salamander_data.zarr",
                  batch_size: int=32, 
                  patch_size: Union[Tuple, List]=(32, 32, 32), 
                  normalizations: List=[], 
                  augmentations: List=[], 
-                 pin_memory: bool=False 
+                 pin_memory: bool=False,
+                 num_workers: int=32
                  ):
         
         super().__init__()
-        self.data_path = "/mnt/data0/home/jnc2161/mbl/registered_salamander_data.zarr"
+        self.data_path = data_path
         self.batch_size = batch_size
-        self.patch_size
-        self.train_data = 
+        self.patch_size = patch_size
+        self.normalizations = normalizations
+        self.augmentations = augmentations
+        self.pin_memory = pin_memory
+        self.num_workers = num_workers
+
+        def train_val_split():
+
 
         def setup(self, stage: str):
+
+            self.dataset = SalBrainDataset(self.data_path, 
+                                           self.patch_size)
+
             if stage == "fit" or stage == "train":
-                ...
+                
             elif stage == "val" or stage == "validate":
                 ...
             elif stage == "predict":
@@ -40,15 +59,45 @@ class SalBrainDataModule(L.LightningDataModule):
 class SalBrainDataset(Dataset):
     """
     For grabbing a sample of brain from the OME-ZARR file
+    - transforms
+    - measure the length of the thing
+    - 
     """
     def __init__(self, 
                  data_path: str, 
-                 normalizations: List=[], 
-                 augmentations: List=[]):
+                 patch_size: Union[List, Tuple],
+                #  normalizations: List=[], 
+                #  augmentations: List=[], 
+                 ):
+        
+        super().__init__(self)
+
         self.data_path = data_path
-        self.normalizations = normalizations
-        self.augmentations = augmentations
+        # self.normalizations = normalizations
+        # self.augmentations = augmentations
+        #self.position_key = "0/0/0/0"
+        #self.volume_shape = (530, 1189, 586)
+        #self.num_samples = 
+        self.patch_size = patch_size
+
+        with open_ome_zarr(
+            self.data_path,
+            mode='r'
+        ) as dataset:
+            self.volume = dataset["0/0/0/0"]
+            self.channel_names = dataset["0/0/0"].channel_names
+            self.bounding_box = get_bounding_box(self, )
+
+
+        self.volume_shape = self.volume.shape
+
+
+    def get_bounding_box(self, brain_mask: np.array):
+
+
 
     def __len__(self):
-        return len()
+        return self.num_samples
+    
+    def _computer_
 
