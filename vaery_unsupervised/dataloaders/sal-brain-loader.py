f11d313d20b490cf12912ccc18c924f7b4e05e08
nicholasjchua <c.nicholas.j@gmail.com>
2025-09-01 11:20:00 -0400

    DataModule and DataSet ready for testing

--- vaery_unsupervised/dataloaders/sal-brain-loader.py
+++ vaery_unsupervised/dataloaders/sal-brain-loader.py
@@ -1,3 +1,5 @@
+import logging
+
 import lightning as L
 from torch import Tensor
 from torch.utils.data import Dataset, DataLoader
@@ -7,9 +9,16 @@ from monai.transforms import RandSpatialCropSamples
 
 from typing import Tuple, Union, List
 
+_logger = logging.getLogger("lightning.pytorch")
+
 class SalBrainDataModule(L.LightningDataModule):
     """
-    logic regarding interaction of data and training routines 
+    Notes:
+    - DataModule initializes DataSet depending on the mode (e.g. train, val, predict)
+    Goals:
+    - Flexible to number of channels: add and exclude in the future
+    - Flexible to spatial resolution
+    - Try different sampling strategies 
     """
     
     def __init__(self, 
@@ -19,53 +28,75 @@ class SalBrainDataModule(L.LightningDataModule):
                  normalizations: List=[], 
                  augmentations: List=[], 
                  pin_memory: bool=False,
-                 num_workers: int=32
+                 num_workers: int=32, 
+                 prefetch_factor: int=2,
+                 persistent_workers: bool=True,
                  ):
         
         super().__init__()
         self.data_path = data_path
         self.batch_size = batch_size
         self.patch_size = patch_size
+
         self.normalizations = normalizations
         self.augmentations = augmentations
+
         self.pin_memory = pin_memory
         self.num_workers = num_workers
-        self.dataset = SalBrainDataset(self.data_path, self.patch_size)
-
-        def train_val_split():
-
-
-        def setup(self, stage: str):
+        self.prefetch_factor = prefetch_factor
+        self.persistent_workers = persistent_workers
+        #self.dataset = SalBrainDataset(self.data_path, self.patch_size)
 
 
+    def setup(self, stage: str):
+        """
+        Initializes the Dataset object based on stage (train, val or predict)
+        """
 
-            if stage == "fit" or stage == "train":
-                
-            elif stage == "val" or stage == "validate":
-                ...
-            elif stage == "predict":
-                ...
-            else:
-                ValueError("Stage must be fit/train, val/validate, or predict.")
-        
-        def train_dataloader(self, ):
-            ...
+        if stage == "fit" or stage == "train":
+            self.dataset = SalBrainDataset(self.data_path, self.batch_size, self.patch_size)
+        elif stage == "val" or stage == "validate":
+            self.dataset = SalBrainDataset(self.data_path, self.batch_size, self.patch_size)
+        elif stage == "predict":
+            raise NotImplementedError("Predict stage not implemented yet.")
+        else:
+            raise ValueError("Stage must be fit/train, val/validate, or predict.")
 
-        def prepare_data(self):
-            """
-            Get samples from the 
-            """
-            
+    def train_dataloader(self, ):
+        return DataLoader(self.dataset, 
+                          shuffle=True, 
+                          batch_size=self.batch_size, 
+                          num_workers=self.num_workers, 
+                          pin_memory=self.pin_memory,
+                          prefetch_factor=self.prefetch_factor,
+                          persistent_workers=self.persistent_workers)
+    
+    def validation_dataloader(self):
+        return DataLoader(self.dataset, 
+                          shuffle=False, 
+                          batch_size=self.batch_size, 
+                          num_workers=self.num_workers, 
+                          pin_memory=self.pin_memory,
+                          prefetch_factor=self.prefetch_factor,
+                          persistent_workers=self.persistent_workers)
+
+
 class SalBrainDataset(Dataset):
     """
-    For grabbing a sample of brain from the OME-ZARR file
-    - transforms
-    - measure the length of the thing
-    - 
+    Handles data IO, prepares batches of data, applies transformations
+    Sampling strategy:
+    - Because the task is unsupervised, it might be okay to sample patches randomly from the volume without
+    explicitly seperating training vs validation patches. The changes of getting the exact same patch is low. 
+    Use of mask:
+    - Mask (of brain) will be used to restrict patch sampling. Initially, just define a bounding box. Might want to
+    try a more sophisticated approach where the centers of each patch are sampled from the mask.
+    - What to do about filling the non-brain pixel values? 
     """
+
     def __init__(self, 
                  data_path: str, 
-                 patch_size: Union[List, Tuple],
+                 batch_size: int, 
+                 patch_size: Union[List, Tuple]
                 #  normalizations: List=[], 
                 #  augmentations: List=[], 
                  ):
@@ -73,37 +104,83 @@ class SalBrainDataset(Dataset):
         super().__init__(self)
 
         self.data_path = data_path
-        # self.normalizations = normalizations
-        # self.augmentations = augmentations
-        #self.position_key = "0/0/0/0"
-        #self.volume_shape = (530, 1189, 586)
-        #self.num_samples = 
         self.patch_size = patch_size
 
         with open_ome_zarr(
             self.data_path,
             mode='r'
         ) as dataset:
-            self.volume = dataset["0/0/0/0"]
+            self.volume = dataset["0/0/0/0"] # dims is (1, C, Z, Y, X), (1, 68, 530, 1189, 585)
             self.channel_names = dataset["0/0/0"].channel_names
 
         self.mask_chan_ind = [i for i, chan in enumerate(self.channel_names) if "mask" in chan]
         assert len(self.mask_chan_ind) == 0, f"Multiple channel names contain the word mask: {self.mask_chan_ind}. Binary mask ambiguous"
         self.mask_chan_ind = self.mask_chan_ind[0]
-        self.bounding_box = get_bounding_box(self, self.volume[0, self.mask_chan_ind])
+        self.bounding_box = self.get_bounding_box(self.volume[0, self.mask_chan_ind])
+        
         self.volume_shape = self.volume.shape
-    
+        self.indices = random_patch_sampler(self.bounding_box, self.patch_size, batch_size*10)
+
     def __len__(self):
-        return self.num_samples
+        return len(self.indices)
+    
+    def __getitem__(self, index):
+        return self.volume[0, 
+                           :, 
+                           self.indices[index][0]:self.indices[index][1], 
+                           self.indices[index][2]:self.indices[index][3], 
+                           self.indices[index][4]:self.indices[index][5]]
 
 
-def get_bounding_box(brainary_mask: np.array) -> Tuple:
-
+def get_bounding_box(brainary_mask: np.array, pad: int=0) -> Tuple:
+    """
+    Use a binary mask to compute a bounding box for the brain in the 
+    space of the volume
+    """
+    volume_shape = brainary_mask.shape
     z_coords, y_coords, x_coords = np.where(brainary_mask > 0)
-    z_min, z_max = np.min(z_coords), np.max(z_coords)
-    y_min, y_max = np.min(y_coords), np.max(y_coords)
-    x_min, x_max = np.min(x_coords), np.max(x_coords)
+    z_min, z_max = np.min(z_coords) - pad, np.max(z_coords) + pad
+    y_min, y_max = np.min(y_coords) - pad, np.max(y_coords) + pad
+    x_min, x_max = np.min(x_coords) - pad, np.max(x_coords) + pad
+
+    bb_size = (z_max - z_min, y_max - y_min, x_max - x_min)
+    print(f"Bounded size {bb_size[0], bb_size[1], bb_size[2]}")
+
+    bbox = (z_min, z_max, y_min, y_max, x_min, x_max)
+    assert all(b >= 0 for b in bbox), f"Bounding box is out of bounds: {bbox}, padding may be too large"
+    assert all(z_max < volume_shape[1] for z_max in bbox[1::2]), f"Bounding box is out of bounds: {bbox}, padding may be too large"
 
     return (z_min, z_max, y_min, y_max, x_min, x_max)
 
 
+def random_patch_sampler(bbox: Tuple, 
+                        patch_size: Union[List, Tuple, int], 
+                        num_samples: int) -> List[Tuple]:
+    """
+    Sample random patches from the given bounding box. 
+    Parameters
+    ----------
+    bbox : Tuple
+        The bounding box from which to sample patches.
+    patch_size : Union[List, Tuple, int]
+        The size of the patches to sample.
+    num_samples : int
+        The number of patches to sample.
+    Returns
+    -------
+    List[Tuple]
+        A list of (z_start, z_end, ... x_start, x_end) ranges. Corresponds to the global coordinates 
+        of the volume.
+    """
+    if isinstance(patch_size, int):
+        patch_size = (patch_size,) * 3
+
+    patches = []
+    for _ in range(num_samples):
+        z_start = np.random.randint(bbox[0], bbox[1] - patch_size[0])
+        y_start = np.random.randint(bbox[2], bbox[3] - patch_size[1])
+        x_start = np.random.randint(bbox[4], bbox[5] - patch_size[2])
+        patches.append((z_start, z_start + patch_size[0],
+                            y_start, y_start + patch_size[1],
+                            x_start, x_start + patch_size[2]))
+    return patches
