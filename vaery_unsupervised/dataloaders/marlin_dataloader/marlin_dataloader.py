5b13e7b6cfe8045e47b85f129ecf5293c0be3a54
gutiloluis <gutiloluis@gmail.com>
2025-09-01 19:50:09 +0000

    marlin_dataloader essentially ready

--- vaery_unsupervised/dataloaders/marlin_dataloader/marlin_dataloader.py
+++ vaery_unsupervised/dataloaders/marlin_dataloader/marlin_dataloader.py
@@ -6,202 +6,230 @@ from pathlib import Path
 from typing import Callable, Literal, Sequence, Optional
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 T = 60
 # H = 150
-HEADPATH = '/mnt/efs/aimbl_2025/student_data/S-GL/'
+HEADPATH = Path('/mnt/efs/aimbl_2025/student_data/S-GL/')
 KEY_FL = 'fluorescence'
 KEY_SEG = 'segmentation'
 
+from monai.transforms import (
+    Flip,
+    Rotate90,
+    Compose,
+    NormalizeIntensity,
+    Lambda,
+)
+
+def center_image(img):
+    h, w = img.shape
+    D = np.max([h, w])# TODO: Pass constants?
+    out = np.zeros((D, D), dtype=img.dtype)
+    y_offset = (D - h) // 2
+    x_offset = (D - w) // 2
+    out[y_offset:y_offset+h, x_offset:x_offset+w] = img
+    return out
+
+
+
+# shift_in_both_chan
+MEAN_OVER_DATASET = 13
+STD_OVER_DATASET = 11
+transform = Compose([
+    Flip(spatial_axis=0),
+    Rotate90(spatial_axes=(0, 2)),
+])
+
+transforms = Compose([
+    NormalizeIntensity(
+        subtrahend=MEAN_OVER_DATASET,
+        divisor=STD_OVER_DATASET,
+        nonzero=False,
+        channel_wise=False,
+        dtype = np.float32,
+    ),
+    Lambda(func=center_image,
+           inv_func=None,
+           track_meta=True)
+])
+
 class MarlinDataset(Dataset):
     def __init__(self,
-                 data_path: Path,
-                 metadata_path: str,
-                 train_mode:bool=True,
-                 split_ratio: float = 0.8,
-                #  train_image_width: int,
-                #  sampling_type: str = None,
+                 data_path: str,
+                 metadata: pd.DataFrame,
                  transform: Optional[Callable] = None):
+        super().__init__()
         self.data_path = data_path
-        self.metadata_path = metadata_path
-        self.train_mode = train_mode
-        self.split_ratio = split_ratio
-        # self.train_image_width = train_image_width
-        # self.sampling_type = sampling_type
-        self.metadata = pd.read_pickle(metadata_path)
+        self.metadata = metadata
         self.transform = transform
-
-        indices_all = self.metadata.index.tolist()
-        np.random.shuffle(indices_all) # TODO
-        self.train_indices = indices_all[:int(len(indices_all) * self.split_ratio)]
-        self.val_indices = indices_all[int(len(indices_all) * self.split_ratio):]
-        if self.train_mode:
-            self.metadata = self.metadata.iloc[train_indices]
-        else:
-            self.metadata = self.metadata.iloc[val_indices]
-
+        
     def __len__(self):
         return len(self.metadata)
-
-    def center_image(self, img):
-            h, w = img.shape
-            D = np.max([h, w])
-            out = np.zeros((D, D), dtype=img.dtype)
-            y_offset = (D - h) // 2
-            x_offset = (D - w) // 2
-            out[y_offset:y_offset+h, x_offset:x_offset+w] = img
-            return out
+ # TODO make this a transform
 
     def __getitem__(self, index: int):
-        
         metadata_sample = self.metadata.iloc[index]
-        print(metadata_sample[['gene_id', 'oDEPool7_id', 'grna_file_trench_index']])
+        # Get indices to find files
         gene_id, grna_id, grna_file_trench_index, timepoint = metadata_sample[['gene_id', 'oDEPool7_id', 'grna_file_trench_index', 'timepoints']]
 
-        # Using only the last timepoint in the meantime
+        # Using only the next timepoint
         filename = self.data_path / f"{gene_id}/{grna_id}.hdf5"
         with h5py.File(filename, 'r') as f:
             img_fl_anchor = f[KEY_FL][grna_file_trench_index, timepoint]
-            img_fl_pos = f[KEY_FL][grna_file_trench_index, (timepoint+1)%T]
+            img_fl_pos = f[KEY_FL][grna_file_trench_index, (timepoint+1)%T] # NO CORRECTION
             img_seg_anchor = f[KEY_SEG][grna_file_trench_index, timepoint]
-            img_seg_pos = f[KEY_SEG][grna_file_trench_index, (timepoint+1)%T]
+            img_seg_pos = f[KEY_SEG][grna_file_trench_index, (timepoint+1)%T] # NO CORRECTION
 
-          # Set your desired output size
+        # Set your desired output size
+        # TODO DECIDE IF INCLUDING SEG
         img_fl_anchor = self.center_image(img_fl_anchor)
         img_fl_pos = self.center_image(img_fl_pos)
         img_seg_anchor = self.center_image(img_seg_anchor)
         img_seg_pos = self.center_image(img_seg_pos)
 
+        # TODO: Apply transformations!
+        if self.transform:
+            NotImplementedError("Transformations are not implemented")
+
+        # if self.train_mode:
         return {'anchor': img_fl_anchor, 'positive': img_fl_pos}
-#%%
-dataset = MarlinDataset(
-    data_path=Path(HEADPATH+'Ecoli_lDE20_Exps-0-1/'),
-    metadata_path=Path(HEADPATH+'2025-08-31_lDE20_Final_Barcodes_df_Merged_Clustering_expanded.pkl'),
-    transform=None
-)
-#%%
-img_anc, img_pos = dataset[3001].values()
-
-import matplotlib.pyplot as plt
-# Visualize the images
-plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
-plt.imshow(img_anc, cmap='gray')
-plt.title('Anchor Image')
-plt.axis('off')
-plt.subplot(1, 2, 2)
-plt.imshow(img_pos, cmap='gray')
-plt.title('Positive Image')
-plt.axis('off')
-plt.show()
-
-#%%
+        # else:
+            # return {'anchor': img_fl_anchor}
+        
 class MarlinDataModule(L.LightningDataModule):
     def __init__(
         self,
         data_path: str,
         metadata_path: str,
-        batch_size:int,
         split_ratio:int,
+        batch_size:int,
         num_workers:int,
         prefetch_factor:int=2,
-        transforms: list = None#Optional[Callable] = None
+        transforms: list = None
     ):
         super().__init__()
         self.data_path = data_path
-        # parse indices
         self.metadata_path = metadata_path
-        self.metadata = pd.read_pickle(metadata_path)
         self.batch_size = batch_size
-        self.indices = self.metadata.index.tolist()
         self.split_ratio = split_ratio
-    
-        print(self.indices)
-        print(len(self.indices))
+        self.num_workers = num_workers
+        self.prefetch_factor = prefetch_factor
+        self.transforms = transforms
+        COLS_TO_LOAD = ['gene_id', 'oDEPool7_id', 'grna_file_trench_index', 'timepoints']
+        self.metadata = (pd
+            .read_pickle(self.metadata_path)
+            [COLS_TO_LOAD]
+        )
 
-        self.train_indices, self.val_indices = train_test_split(self.indices, test_size=self.split_ratio)
+    def _prepare_data(self):
+        # Logica para abrir la metadta dataframe, filter y shuffle and split
+        # indices_all = self.metadata.index.tolist()
+        indices_shuffled = np.random.permutation(self.metadata.index.tolist())
+        indices_train = indices_shuffled[:int(len(indices_shuffled) * self.split_ratio)]
+        indices_val = indices_shuffled[int(len(indices_shuffled) * self.split_ratio):]
+        self.metadata_train = self.metadata.loc[indices_train]
+        self.metadata_val = self.metadata.loc[indices_val]
 
-    def setup(self, stage: str):
-        if stage == "fit" or stage == "train":
-            self.dataset = MarlinDataset(self.data_paths, self.train_indices)
-        elif stage == "val" or stage == "validate":
-            self.dataset = MarlinDataset(self.data_paths, self.val_indices)
-        elif stage == "predict":
-        NotImplementedError("This method is not implemented")
+    def setup(self, stage):
+        if stage == 'fit': # includes validation
+            self.dataset = MarlinDataset(
+                data_path=self.data_path,
+                metadata=self.metadata_train,
+                transform=...
+            )
+        elif stage == 'validate':
+            self.dataset = MarlinDataset(
+                data_path=self.data_path,
+                metadata=self.metadata_val,
+                transforms=...
+            )
+        else: # 'test' or 'predict'
+            self.dataset = MarlinDataset(
+                data_path=self.data_path,
+                metadata=self.metadata,
+                transform=...
+            )
 
-    def train_dataloader(self):
-        return DataLoader(
-            self.dataset,
-            shuffle=True,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            pin_memory=True,
-            prefetch_factor=self.prefetch_factor)
-
-    def val_dataloader(self):
+    def train_dataloader(self): 
         return DataLoader(
             self.dataset,
             shuffle=False,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             pin_memory=True,
-            prefetch_factor=self.prefetch_factor)
+            prefetch_factor=self.prefetch_factor
+        )
 
-    def predict_dataloader(self):
+    # Whenever called, I need to change mode
+    def val_dataloader(self):
         return DataLoader(
-            self.dataset,
+            dataset=self.dataset,
+            shuffle=False,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             pin_memory=True,
-            prefetch_factor=self.prefetch_factor)
+            prefetch_factor=self.prefetch_factor
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            dataset=self.dataset,
+            shuffle=False,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            prefetch_factor=self.prefetch_factor
+        )
 
 #%%
-
-HEADPATH = '/mnt/efs/aimbl_2025/student_data/S-GL/'
-data_module = DemoDataModule(
-    data_path=Path(HEADPATH+'Ecoli_lDE20_Exps-0-1/'),
-    metadata_path = Path(HEADPATH+'2025-08-31_lDE20_Final_Barcodes_df_Merged_Clustering.pkl'),
-    batch_size=32,
+HEADPATH = Path('/mnt/efs/aimbl_2025/student_data/S-GL/')
+data_module = MarlinDataModule(
+    data_path=HEADPATH/'Ecoli_lDE20_Exps-0-1/',
+    metadata_path=HEADPATH/'2025-08-31_lDE20_Final_Barcodes_df_Merged_Clustering_expanded.pkl',
+    split_ratio=0.8,
+    batch_size=1024,
     num_workers=4,
     prefetch_factor=2,
     transforms=[...]
 )
 
 #%%
-data_module.indices
-
+data_module._prepare_data()
 #%%
-    def prepare_data(self):
-        return None
-    def setup(self, stage:str):
-        if stage == "fit":
-            print(0)
-            # self.train_dataset = MyDataset(train=True)
-        elif stage == "validate":
-            print(1)
-            # self.val_dataset = MyDataset(train=False)
-        
-
-    def train_dataloader(self):
-
-def __get_item(self, index):
-    sample = open_ome_zarr()
-
-    if self.transform:
-        NotImplementedError("")
-        # self.transform a list of transforms [crop, rotation, flip, normalize]
-        # List of transforms
-        sample - self.transform(sample)
-
-        # return anchor, and positive, and positive augmentation.
-
-
-    return sample
+data_module.setup(stage='fit')
 
 # Transforms
  # Reflection over either axis
  # Shuffling of cells over the trench?
  # 
+# %%
+img_anchor, img_pos =data_module.dataset[45000].values()
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+ax[0].imshow(img_anchor, cmap='gray')
+ax[0].set_title('Anchor Image')
+ax[0].axis('off')
+ax[1].imshow(img_pos, cmap='gray')
+ax[1].set_title('Positive Image')
+ax[1].axis('off')
+plt.show()
+#%%
+
+# transforms(img_anchor)
+center_image(img_anchor)
+#%%
+metadata_sample = data_module.dataset.metadata.iloc[3001]
+gene_id, grna_id, grna_file_trench_index, timepoint = metadata_sample[['gene_id', 'oDEPool7_id', 'grna_file_trench_index', 'timepoints']]
+filename = data_module.dataset.data_path / f"{gene_id}/{grna_id}.hdf5"
+with h5py.File(filename, 'r') as f:
+    imgs = f[KEY_FL][grna_file_trench_index]
+    print(imgs.shape)
+
+#%%
+metadata = pd.read_pickle(HEADPATH / '2025-08-31_lDE20_Final_Barcodes_df_Merged_Clustering_expanded.pkl')
+metadata
--- vaery_unsupervised/dataloaders/marlin_dataloader/utils.py
+++ vaery_unsupervised/dataloaders/marlin_dataloader/utils.py
@@ -1,5 +1,7 @@
+#%%
 import pandas as pd
-
+import numpy as np
+import h5py
 
 def expand_metadata_on_time(
     metadata: pd.DataFrame,
@@ -18,5 +20,69 @@ def expand_metadata_on_time(
         .astype(
             {'Experiment #': 'category'}
         )
+
     )
+
+    metadata_expanded.index.name = 'gene_grna_trench_timepoint_index'
     return metadata_expanded
+
+def compute_mean_and_std_over_sample(
+    data_path:str,
+    metadata:pd.DataFrame,
+):
+    COLS_TO_KEEP = ['gene_grna_trench_index', 'gene_id', 'oDEPool7_id', 'grna_file_trench_index']
+    
+    metadata_only_gene_grnas = (metadata
+        .loc[:, COLS_TO_KEEP]
+        .groupby(['gene_id', 'oDEPool7_id'])
+        .first()
+        .reset_index()
+    )
+
+    gene_ids_sample = np.random.choice(metadata_only_gene_grnas['gene_id'].unique(), size=20, replace=False)
+    metadata_only_gene_sample = metadata_only_gene_grnas[metadata_only_gene_grnas['gene_id'].isin(gene_ids_sample)]
+    # Go through all images indexed on that table
+
+    n_images = 0
+    weighted_mean = 0
+    weighted_std = 0
+
+    for i, row in metadata_only_gene_sample.iterrows():
+        print(i)
+        gene_id = row['gene_id']
+        grna_id = row['oDEPool7_id']
+        filename = data_path / f"{gene_id}/{grna_id}.hdf5"
+        with h5py.File(filename, 'r') as f:
+            img_fl = f[KEY_FL]
+            mean_ = np.mean(img_fl)
+            std_ = np.std(img_fl)
+            n_images += img_fl.shape[0]
+            weighted_mean += mean_ * n_images
+            weighted_std += std_ * n_images
+
+    mean_sample = weighted_mean / n_images
+    std_sample = weighted_std / n_images
+    return n_images, mean_sample, std_sample
+
+#%%
+from pathlib import Path
+HEADPATH = Path('/mnt/efs/aimbl_2025/student_data/S-GL/')
+KEY_FL = 'fluorescence'
+KEY_SEG = 'segmentation' 
+metadata = pd.read_pickle(HEADPATH / '2025-08-31_lDE20_Final_Barcodes_df_Merged_Clustering_expanded.pkl')
+
+#%%
+n_images, mean_sample, std_sample = compute_mean_and_std_over_sample(
+    data_path=HEADPATH / 'Ecoli_lDE20_Exps-0-1/',
+    metadata=metadata
+)
+print(n_images, mean_sample, std_sample)
+# %%
+N_IMAGES = 15290
+WEIGHTED_MEAN = 200183.2210227729
+WEIGHTED_STD = 175738.43323474968
+
+SAMPLED_MEAN = WEIGHTED_MEAN/N_IMAGES
+SAMPLED_STD = WEIGHTED_STD/N_IMAGES
+
+print(SAMPLED_MEAN, SAMPLED_STD)
