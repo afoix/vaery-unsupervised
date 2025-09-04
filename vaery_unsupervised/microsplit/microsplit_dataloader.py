da279184e668adc83376403412326ad7471e9fa7
federico-carrara <federico.carrara@fht.org>
2025-09-04 03:48:36 +0000

    refac: split dataset for training and inference

--- vaery_unsupervised/microsplit/microsplit_dataloader.py
+++ vaery_unsupervised/microsplit/microsplit_dataloader.py
@@ -13,14 +13,13 @@ from monai.transforms import Compose, RandSpatialCrop
 from utils import extract_overlapping_patches_grid
 
 
-class MicroSplitHCSDataset(Dataset):
+class MicroSplitHCSTrainingDataset(Dataset):
     def __init__(
         self,
         positions: list[Position],
         source_channel_names: list[str],
         crop_size: tuple[int, int] = (128, 128),
         crops_per_position: int = 4,
-        random_cropping: bool = True,
         transforms: list[Callable] = None,
         mix_coeff_range: tuple[float, float] | None = None,
     ) -> None:
@@ -36,7 +35,6 @@ class MicroSplitHCSDataset(Dataset):
         self.source_channel_names = source_channel_names
         self.crop_size = crop_size
         self.crops_per_position = crops_per_position
-        self.random_cropping = random_cropping
         self.transforms = Compose(transforms) if transforms else None
         self.mix_coeff_range = mix_coeff_range
 
@@ -60,30 +58,80 @@ class MicroSplitHCSDataset(Dataset):
 
         # convert to tensor
         multi_ch_img = torch.from_numpy(multi_ch_img).float()
+        
+        # apply transforms if any
+        if self.transforms:
+            multi_ch_img = self.transforms(multi_ch_img)
 
-        # crop image (random or grid)
-        patches_info = {
-            "coords": None,
-            "index": index
-        }
-        if self.random_cropping:
-            cropper = RandSpatialCrop(roi_size=self.crop_size)
-            multi_ch_img = cropper(multi_ch_img)
-            patches_info["coords"] = None  # random cropping does not have fixed coordinates
-        else:
-            multi_ch_img, coords = extract_overlapping_patches_grid(
-                img=multi_ch_img.numpy(),
-                patch_size=self.crop_size,
-                overlap=(self.crop_size[0] // 2, self.crop_size[1] // 2)
-            )
-            patches_info["coords"] = coords
-
+        # crop image
+        cropper = RandSpatialCrop(roi_size=self.crop_size)
+        multi_ch_img = cropper(multi_ch_img)
+        
         # get superimposed image
         if self.mix_coeff_range is not None:
             mix_coeffs = torch.empty(multi_ch_img.shape[0]).uniform_(*self.mix_coeff_range)
         else:
             mix_coeffs = torch.ones(multi_ch_img.shape[0])
-        mixed_img = torch.mean(multi_ch_img * mix_coeffs[..., None, None], dim=-3, keepdim=True)
+        mixed_img = torch.mean(multi_ch_img * mix_coeffs[:, None, None], dim=0, keepdim=True)
+
+        return mixed_img, multi_ch_img
+    
+    def __len__(self):
+        return len(self.all_positions)
+
+
+class MicroSplitHCSInferenceDataset(Dataset):
+    def __init__(
+        self,
+        positions: list[Position],
+        source_channel_names: list[str],
+        crop_size: tuple[int, int] = (128, 128),
+        crops_per_position: int = 4,
+    ) -> None:
+        """
+        Parameters:
+            positions: List of iohub.ngff.Position objects
+            source_channel_names: Channel names to use
+            crop_size: Size of random crops
+            crops_per_position: Number of crops to generate per position per epoch
+            
+        """
+        self.positions = positions
+        self.source_channel_names = source_channel_names
+        self.crop_size = crop_size
+        self.crops_per_position = crops_per_position
+
+        # get all positions from file
+        self.all_positions = [pos for pos in positions for _ in range(self.crops_per_position)]
+
+    def __getitem__(self, index):
+        # get multichannel image
+        position = self.all_positions[index]
+        source_channel_indices = [position.channel_names.index(ch) for ch in self.source_channel_names]
+        multi_ch_img = position[0].oindex[0, source_channel_indices, 0]
+
+        # quantile normalization on the full image
+        max_per = np.quantile(multi_ch_img, 0.99, axis=(1, 2), keepdims=True)
+        min_per = np.quantile(multi_ch_img, 0.01, axis=(1, 2), keepdims=True)
+        multi_ch_img = (multi_ch_img - min_per) / (max_per - min_per)
+
+        # filter out empty patches setting them to zeros
+        if np.isnan(multi_ch_img).any():
+            multi_ch_img = np.nan_to_num(multi_ch_img, nan=0.0, posinf=1.0, neginf=0.0)
+
+        # convert to tensor
+        multi_ch_img = torch.from_numpy(multi_ch_img).float()
+
+        # crop image in a grid of overlapping patches
+        multi_ch_img, coords = extract_overlapping_patches_grid(
+            img=multi_ch_img.numpy(),
+            patch_size=self.crop_size,
+            overlap=(self.crop_size[0] // 2, self.crop_size[1] // 2)
+        )
+        patches_info = {"coords": coords, "idx": index}
+
+        # get superimposed image
+        mixed_img = torch.mean(multi_ch_img, dim=1, keepdim=True)
 
         return mixed_img, multi_ch_img, patches_info
     
@@ -136,32 +184,27 @@ class MicroSplitHCSDataModule(pl.LightningDataModule):
         num_test_fovs = len(positions) - num_train_fovs - num_val_fovs
 
         # initialize datasets
-        self.train_dataset = MicroSplitHCSDataset(
+        self.train_dataset = MicroSplitHCSTrainingDataset(
             positions= positions[:num_train_fovs],
             crop_size=self.crop_size,
             crops_per_position=self.crops_per_position,
-            random_cropping=self.random_cropping,
             source_channel_names= ['mito', 'er', 'nuclei'],
             transforms=self.augmentations,
             mix_coeff_range=self.mix_coeff_range
         )
-        self.val_dataset = MicroSplitHCSDataset(
+        self.val_dataset = MicroSplitHCSTrainingDataset(
             positions=positions[num_train_fovs:(num_train_fovs + num_val_fovs)],
             crop_size=self.crop_size,
             crops_per_position=self.crops_per_position,
-            random_cropping=False,
             source_channel_names=['mito', 'er', 'nuclei'],
             transforms=None,
             mix_coeff_range=None
         )
-        self.test_dataset = MicroSplitHCSDataset(
+        self.test_dataset = MicroSplitHCSInferenceDataset(
             positions=positions[(num_train_fovs + num_val_fovs):],
             crop_size=self.crop_size,
             crops_per_position=self.crops_per_position,
-            random_cropping=False,
             source_channel_names=['mito', 'er', 'nuclei'],
-            transforms=None,
-            mix_coeff_range=None
         )
         
     def test_data_collate_fn(self, batch):
@@ -190,7 +233,6 @@ class MicroSplitHCSDataModule(pl.LightningDataModule):
             batch_size=self.batch_size, 
             shuffle=False,
             num_workers=self.num_workers,
-            collate_fn=self.test_data_collate_fn
         )
         
     def test_dataloader(self):
